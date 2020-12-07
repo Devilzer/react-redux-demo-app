e86030c7db4be1544219bd002813098d798e34eb
Devilzer <jenadeepak724@gmail.com>
2020-12-08 01:45:04 +0530

    setup

---
+++ src/components/Cakecontainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function cakeContainer() {
+    return (
+        <div>
+            <h1>Number of Cakes</h1>
+            <button>buy cake</button>
+        </div>
+    )
+}
+
+export default cakeContainer;
