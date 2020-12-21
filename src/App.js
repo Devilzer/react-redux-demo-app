ff63e1b6961e795c00cb7f5f536dc56cf0887ffa
Devilzer <jenadeepak724@gmail.com>
2020-12-21 21:19:31 +0530

    add store

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import Cakecontainer from "./components/Cakecontainer";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+
 function App() {
   return (
-    <div className="App">
-      <Cakecontainer/>
-    </div>
-  );
+    <Provider store={store}>
+      <div className="App">
+        <Cakecontainer/>
+      </div>
+    </Provider>
+    );
 }
 
 export default App;
